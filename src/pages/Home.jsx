9c88364abce738a9514243f48ea6ee8951b71dd2
Tatiana Privezentseva <severny.island@gmail.com>
2023-06-26 17:23:51 -0700

    update nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 // import { useEffect, useState } from "react";
 import "./Home.scss";
+import { PiLinkedinLogo } from "react-icons/pi";
+import { VscGithub } from "react-icons/vsc";
 
 function Home() {
   //   const [count, setCount] = useState(0)
@@ -12,21 +14,28 @@ function Home() {
     <>
       <nav className="nav">
         <div id="logo" className="nav__section">
-          <a href="#"></a>
+          <a href="#">T.</a>
         </div>
 
         <div id="link" className="nav__section">
           <a href="#">About</a>
           <a href="#">Work</a>
+          <a href="#">CV</a>
         </div>
 
         <div id="social" className="nav__section">
-          <a href="#"></a>
-          <a href="#"></a>
-          <a href="#"></a>
+          <a href="#">
+            <VscGithub size={21} />
+          </a>
+          <a href="#">
+            <PiLinkedinLogo size={24} />
+          </a>
         </div>
         <div id="contact" className="nav__section">
           <a href="#">Get in Touch</a>
+        </div>
+        <div id="menu" className="nav__section">
+          <a href="#">Menu</a>
         </div>
       </nav>
     </>
