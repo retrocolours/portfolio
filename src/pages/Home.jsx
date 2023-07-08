539753d627716dff0b77d73dc30caf718f577431
Jackie Chui <severny.island@gmail.com>
2023-07-07 21:11:25 -0700

    added border decoration to the site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -99,71 +99,73 @@ function Home() {
 
   return (
     <>
-    <div className="background">
-      <nav className="nav">
-        <div id="logo" className="nav__section">
-          <a href="#">T.</a>
-        </div>
+    <div className="site-container">
+      <div className="site-inner-container">
+        <nav className="nav">
+          <div id="logo" className="nav__section">
+            <a href="#">T.</a>
+          </div>
 
-        <div id="link" className="nav__section">
-          <a href="#">About</a>
-          <a href="#">Work</a>
-          <a href="#">CV</a>
-        </div>
+          <div id="link" className="nav__section">
+            <a href="#">About</a>
+            <a href="#">Work</a>
+            <a href="#">CV</a>
+          </div>
 
-        <div id="social" className="nav__section">
-          <a href="#">
-            <VscGithub size={21} />
-          </a>
-          <a href="#">
-            <PiLinkedinLogo size={24} />
-          </a>
-        </div>
-        <div id="contact" className="nav__section">
-          <a href="#">Get in Touch</a>
-        </div>
-        <div id="menu" className="nav__section">
-          <a href="#">Menu</a>
-        </div>
-      </nav>
+          <div id="social" className="nav__section">
+            <a href="#">
+              <VscGithub size={21} />
+            </a>
+            <a href="#">
+              <PiLinkedinLogo size={24} />
+            </a>
+          </div>
+          <div id="contact" className="nav__section">
+            <a href="#">Get in Touch</a>
+          </div>
+          <div id="menu" className="nav__section">
+            <a href="#">Menu</a>
+          </div>
+        </nav>
 
-      <main className="main">
-        {groups.map((group) => (
-          <article
-            className="article"
-            key={group.index}
-            data-index={group.index}
-            data-status={group.status}
-          >
-            <div className="article__section article__section--image">
-              {group.content.image}
-            </div>
-            <div className="article__section article__section--description">
-              {group.content.description}
-            </div>
-            <div className="article__section article__section--title">
-              <img className="article__image" src={group.content.imageSrc} />
-            </div>
-            <div className="article__section article__section--nav">
-              <button
-                className="article__button"
-                type="button"
-                onClick={handleLeftClick}
-              >
-                <PiArrowBendUpLeftDuotone size={24} />
-              </button>
-              <button
-                className="article__button"
-                type="button"
-                onClick={handleRightClick}
-              >
-                <PiArrowBendUpRightDuotone size={24} />
-              </button>
-            </div>
-          </article>
-        ))}
-      </main>
+        <main className="main">
+          {groups.map((group) => (
+            <article
+              className="article"
+              key={group.index}
+              data-index={group.index}
+              data-status={group.status}
+            >
+              <div className="article__section article__section--image">
+                {group.content.image}
+              </div>
+              <div className="article__section article__section--description">
+                {group.content.description}
+              </div>
+              <div className="article__section article__section--title">
+                <img className="article__image" src={group.content.imageSrc} />
+              </div>
+              <div className="article__section article__section--nav">
+                <button
+                  className="article__button"
+                  type="button"
+                  onClick={handleLeftClick}
+                >
+                  <PiArrowBendUpLeftDuotone size={24} />
+                </button>
+                <button
+                  className="article__button"
+                  type="button"
+                  onClick={handleRightClick}
+                >
+                  <PiArrowBendUpRightDuotone size={24} />
+                </button>
+              </div>
+            </article>
+          ))}
+        </main>
       </div>
+    </div>
     </>
   );
 }
