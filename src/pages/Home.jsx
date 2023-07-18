baa8d189721fc606c0a76bf6d1ae44fd6aa87abd
Tatiana Privezentseva <severny.island@gmail.com>
2023-07-18 16:50:02 -0700

    start working on the about section, add content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -138,6 +138,33 @@ function Home() {
                   <a href="#">Menu</a>
                 </div>
               </div>
+              <div className="about__content">
+                <h1 className="about__title">Tatiana Privezentseva</h1>
+                <h2 className="about__subtitle">Full-Stack Web Developer</h2>
+                <p className="about__text">
+                  Contrary to popular belief, Lorem Ipsum is not simply random
+                  text. It has roots in a piece of classical Latin literature
+                  from 45 BC, making it over 2000 years old. Richard McClintock,
+                  a Latin professor at Hampden-Sydney College in Virginia,
+                  looked up one of the more obscure Latin words, consectetur,
+                  from a Lorem Ipsum passage, and going through the cites of the
+                  word in classical literature, discovered the undoubtable
+                  source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of
+                  "de Finibus Bonorum et Malorum" (The Extremes of Good and
+                  Evil) by Cicero, written in 45 BC. This book is a treatise on
+                  the theory of ethics, very popular during the Renaissance. The
+                  first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
+                  comes from a line in section 1.10.32.
+                </p>
+              </div>
+              <div className="about__social">
+                <button className="about__about" type="button">
+                  About
+                </button>
+                <button className="about__contact" type="button">
+                  Contact
+                </button>
+              </div>
             </section>
             <section className="projects">
               {groups.map((group) => (
