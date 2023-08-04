8a5f4937739186eb9067c90ced2a9e16f408a6e9
Tatiana Privezentseva <severny.island@gmail.com>
2023-08-03 21:53:43 -0700

    add grain effect to the background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -162,6 +162,7 @@ function Home() {
 
   return (
     <>
+      <div class="grain"></div>
       <div className="site-container" onMouseMove={handleMouseMove}>
         <div className="test"></div>
         <div className="site-inner-container">
@@ -193,8 +194,6 @@ function Home() {
           </nav> */}
 
           <main className="main">
-            {/* <div class="grain"></div> */}
-
             <section className="about">
               <div className="about__nav">
                 <div className="about__logo">
